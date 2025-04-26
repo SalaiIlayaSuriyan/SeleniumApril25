a225f9794bc51f28975ee7d9354aef0ad05a1958
SalaiIlayaSuriyan <salaiilayasuriyan@gmail.com>
2025-04-27 01:08:53 +0530

    Add files via upload
    Day 1 assignment

---
+++ day1project1/Chrome.java
@@ -0,0 +1,18 @@
+package day1project1;
+
+public class Chrome {
+public static void main(String[] args) {
+	float version = 91.0f;
+	String developer = "Google";
+	boolean isBeta = false;
+	int releaseyear = 2008;
+	char shortcutKey = 'c';
+	System.out.println(version);
+	System.out.println(developer);
+	System.out.println(isBeta);
+	System.out.println(2008);
+	System.out.println(shortcutKey);
+	
+	
+}
+}
---
+++ day1project1/project1.java
@@ -0,0 +1,8 @@
+package day1project1;
+
+public class project1 {
+public static void main(String[] args) {
+	System.out.println("Hello everyone");
+	System.out.println("Welcome to testleaf");	
+}
+}
